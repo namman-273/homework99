fc16c40f77eca72345f421f1631976b000955d75
namman-273 <25021902@vnu.edu.vn>
2026-05-02 15:24:00 +0700

    up

---
+++ src/main/java/com/App.java
@@ -0,0 +1,11 @@
+package com;
+
+public class App {
+    public static void main(String[] args) {
+        // Lỗi: Thụt lề sai, tên biến không đúng chuẩn camelCase, thiếu khoảng trắng
+        int so_Nguyen_1 = 10;
+        if(so_Nguyen_1>5){
+            System.out.println("Checkstyle se bat loi o day");
+        }
+    }
+}
